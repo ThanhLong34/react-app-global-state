a6e63c2bb1c6191e0c2bb1c76e38d0bb1ca14bd5
DragonDev <thanhlongedu0304@gmail.com>
2023-02-24 15:59:21 +0700

    first commit

---
+++ NOTE.md
@@ -0,0 +1,9 @@
+    useState
+    	1. Init state
+    	2. Actions
+
+    useReducer
+    	1. Init state
+    	2. Actions
+    	3. Reducer
+    	4. Dispatch
--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef } from "react";
+import { useStore, actions } from "./store";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [state, dispatch] = useStore();
+	const { todos, todoInput } = state;
+	const todoRef = useRef();
+
+	function handleAddTodo() {
+		dispatch(actions.addTodo(todoInput));
+		dispatch(actions.setTodoInput(""));
+		todoRef && todoRef.current.focus();
+	}
+
+	return (
+		<div className="App">
+			<input
+				ref={todoRef}
+				type="text"
+				value={todoInput}
+				placeholder="Enter todo..."
+				onChange={(e) => dispatch(actions.setTodoInput(e.target.value))}
+			/>
+
+			<button onClick={handleAddTodo}>Add</button>
+
+			<ul>
+				{todos && todos.map((todo, index) => <li key={index}>{todo}</li>)}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
